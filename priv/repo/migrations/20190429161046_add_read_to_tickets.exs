deb38c4579179df08cb4d14af7ee85c628b8df5f
iladiro <dirosa.ilaria@gmail.com>
2019-04-29 19:01:44 +0200

    add read_by field on tickets table

--- priv/repo/migrations/20190429161046_add_read_to_tickets.exs
+++ priv/repo/migrations/20190429161046_add_read_to_tickets.exs
@@ -2,7 +2,7 @@ defmodule BraccoPubSub.Repo.Migrations.AddReadToNotes do
   use Ecto.Migration
 
   def change do
-    alter table(:ticket) do
+    alter table(:tickets) do
       add(:read_by, {:array, :integer}, default: [])
     end
   end
