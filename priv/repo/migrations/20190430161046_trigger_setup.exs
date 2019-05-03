0a155d1f6b3529c93fd1d8687a89971487ed7b53
iladiro <dirosa.ilaria@gmail.com>
2019-05-03 16:59:46 +0200

    replace dist

--- priv/repo/migrations/20190430161046_trigger_setup.exs
+++ priv/repo/migrations/20190430161046_trigger_setup.exs
@@ -3,122 +3,29 @@ defmodule BraccoPubSub.Repo.Migrations.TriggerSetup do
 
   def change do
     execute """
-      CREATE OR REPLACE FUNCTION notify_ticket_changes()
-      RETURNS trigger AS $$
-      BEGIN
-        IF (TG_OP = 'DELETE') THEN
-          PERFORM pg_notify(
-            'tickets_changed',
-            json_build_object(
-              'operation', TG_OP,
-              'table', 'tickets',
-              'record', row_to_json(OLD)
-            )::text
-          );
-          RETURN OLD;
-        ELSE
-          PERFORM pg_notify(
-            'tickets_changed',
-            json_build_object(
-              'operation', TG_OP,
-              'table', 'tickets',
-              'record', row_to_json(NEW)
-            )::text
-          );
-          RETURN NEW;
-        END IF;
-
-        RETURN NULL;
-
-      END;
-      $$ LANGUAGE plpgsql;
+      DROP TRIGGER tickets_changed ON tickets;
     """
 
     execute """
       CREATE TRIGGER tickets_changed
-      AFTER INSERT OR DELETE OR UPDATE OF updated_at ON tickets
+      AFTER INSERT OR DELETE
+      ON tickets
       FOR EACH ROW
       EXECUTE PROCEDURE notify_ticket_changes();
     """
 
     execute """
-      CREATE OR REPLACE FUNCTION notify_comment_changes()
-      RETURNS trigger AS $$
-      BEGIN
-        IF (TG_OP = 'DELETE') THEN
-          PERFORM pg_notify(
-            'comments_changed',
-            json_build_object(
-              'operation', TG_OP,
-              'table', 'comments',
-              'record', row_to_json(OLD)
-            )::text
-          );
-          RETURN OLD;
-        ELSE
-          PERFORM pg_notify(
-            'comments_changed',
-            json_build_object(
-              'operation', TG_OP,
-              'table', 'comments',
-              'record', row_to_json(NEW)
-            )::text
-          );
-          RETURN NEW;
-        END IF;
-
-        RETURN NULL;
-
-      END;
-      $$ LANGUAGE plpgsql;
-    """
-
-    execute """
-      CREATE TRIGGER comments_changed
-      AFTER INSERT OR UPDATE OR DELETE
-      ON comments
+      CREATE TRIGGER tickets_updated
+      AFTER UPDATE
+      ON tickets
       FOR EACH ROW
-      EXECUTE PROCEDURE notify_comment_changes();
+      WHEN (OLD.updated_at IS DISTINCT FROM NEW.updated_at)
+      EXECUTE PROCEDURE notify_ticket_changes();
     """
 
     execute """
-      CREATE OR REPLACE FUNCTION notify_document_changes()
-      RETURNS trigger AS $$
-      BEGIN
-        IF (TG_OP = 'DELETE') THEN
-          PERFORM pg_notify(
-            'documents_changed',
-            json_build_object(
-              'operation', TG_OP,
-              'table', 'documents',
-              'record', row_to_json(OLD)
-            )::text
-          );
-          RETURN OLD;
-        ELSE
-          PERFORM pg_notify(
-            'documents_changed',
-            json_build_object(
-              'operation', TG_OP,
-              'table', 'documents',
-              'record', row_to_json(NEW)
-            )::text
-          );
-          RETURN NEW;
-        END IF;
-
-        RETURN NULL;
-
-      END;
-      $$ LANGUAGE plpgsql;
-    """
-
-    execute """
-      CREATE TRIGGER documents_changed
-      AFTER INSERT OR UPDATE OR DELETE
-      ON documents
-      FOR EACH ROW
-      EXECUTE PROCEDURE notify_document_changes();
+      DROP TRIGGER comments_changed ON comments;
+      DROP FUNCTION notify_comment_changes();
     """
   end
 end
