ea2713199a40c4aef106928f1e2f159a28efe30d
ilaria.dirosa <dirosa.ilaria@gmail.com>
2019-05-14 10:52:13 +0200

    create migration, add commented_at field

---
+++ priv/repo/migrations/20190514161046_add_commented-at_to_tickets.exs
@@ -0,0 +1,9 @@
+defmodule BraccoPubSub.Repo.Migrations.AddReadToNotes do
+  use Ecto.Migration
+
+  def change do
+    alter table(:tickets) do
+      add(:commented_at, :timestamp, default: fragment("now()"))
+    end
+  end
+end
