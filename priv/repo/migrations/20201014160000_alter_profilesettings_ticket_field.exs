989a8921bd7948358ffeb0267505d4ade76bdcfe
iladiro <dirosa.ilaria@gmail.com>
2020-10-30 08:06:05 +0100

    ok

--- priv/repo/migrations/20201014160000_alter_profilesettings_ticket_field.exs
+++
@@ -1,9 +0,0 @@
-defmodule BraccoPubSub.Repo.Migrations.AlterProfilesettingsTicketField do
-  use Ecto.Migration
-
-  def change do
-    alter table(:profile_settings) do
-      modify(:ticket, :json, default: "{query_default: false, query_items: {}}")
-    end  
-  end
-end
